47a13ee7411adabadb05cf214c08e30952e94736
bob.pan <bob.pan@hzleaper.com>
2019-04-28 10:08:02 +0800

    add terminal 串口终端

---
+++ terminal/src/mainwindow.h
@@ -0,0 +1,46 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QSerialPort>
+
+class QLabel;
+
+namespace Ui {
+class MainWindow;
+}
+
+class Console;
+class SettingsDialog;
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private slots:
+    void openSerialPort();
+    void closeSerialPort();
+    void about();
+    void writeData(const QByteArray &data);
+    void readData();
+
+    void handleError(QSerialPort::SerialPortError error);
+
+private:
+    void initActionsConnections();
+
+private:
+    void showStatusMessage(const QString &message);
+
+    Ui::MainWindow *ui;
+    QLabel *status;
+    Console *console;
+    SettingsDialog *settings;
+    QSerialPort *serial;
+};
+
+#endif // MAINWINDOW_H
